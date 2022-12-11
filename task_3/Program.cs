a97cf511bf9ef6a6058d1e37c8622b5291c1d27c
Zheshard <antonzestkov489@gmail.com>
2022-12-11 23:58:50 +0300

    Added array creation method (task_3)

---
+++ task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+System.Console.WriteLine(string.Join(" | ", CreateArray(10, 1000)));
+
+int[] CreateArray(int leftRange, int rightRange)
+{
+	int[] randomArray = new int[8];
+	Random randNumber = new Random();
+	for (int i = 0; i < randomArray.Length; i++)
+	{
+		randomArray[i] = randNumber.Next(leftRange, rightRange);
+	}
+	return randomArray;
+}
