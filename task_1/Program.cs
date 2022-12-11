01443db45079453f0904c0c9a9e76b0a3c93d68f
Zheshard <antonzestkov489@gmail.com>
2022-12-11 22:40:12 +0300

    Implemented integer input method (task_1)

---
+++ task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (реализована с использованием цикла)
+int numberA = inputNumber("Введите число А: ");
+int numberB = inputNumber("Введите число B: ");
+
+int inputNumber(string invitationText)
+{
+	int number;
+	while (true)
+	{
+		Console.Write(invitationText);
+		string numString = Console.ReadLine();
+		if (int.TryParse(numString, out number))
+		{
+			break;
+		}
+		Console.WriteLine("Введено неверное значение. Введите число: ");
+	}
+	return number;
+}
