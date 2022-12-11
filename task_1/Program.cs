d1cc5b6dccd6a695f7c96ca535f9dee24fadf3eb
Zheshard <antonzestkov489@gmail.com>
2022-12-11 22:55:00 +0300

    Implemented a method for raising a number to a power (task_1)

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,6 +1,17 @@
 ﻿// Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (реализована с использованием цикла)
 int numberA = inputNumber("Введите число А: ");
 int numberB = inputNumber("Введите число B: ");
+System.Console.WriteLine(Pow(numberA, numberB));
+
+int Pow(int number, int degreeOf)
+{
+	int resultPow = 1;
+	for (int i = 1; i <= degreeOf; i++)
+	{
+		resultPow = resultPow * number;
+	}
+	return resultPow;
+}
 
 int inputNumber(string invitationText)
 {
