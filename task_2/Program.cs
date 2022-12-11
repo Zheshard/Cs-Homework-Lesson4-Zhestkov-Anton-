db157a2e7bc8a380a10260d2d154a1fd7835c4e0
Zheshard <antonzestkov489@gmail.com>
2022-12-11 23:03:45 +0300

    Implemented integer input method (task_2)

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
-Console.WriteLine("Hello, World!");
+int number = inputNumber("Введите число: ");
+
+int inputNumber(string invitationText)
+{
+	int number;
+	while (true)
+	{
+		Console.Write(invitationText);
+		string numString = Console.ReadLine();
+		if (int.TryParse(numString, out number))
+		{
+			break;
+		}
+		Console.WriteLine("Введено неверное значение. Введите число: ");
+	}
+	return number;
+}
